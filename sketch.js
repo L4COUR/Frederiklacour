d9459ca3fe6d4e4f800724bfaa1f082191906fb5
Frederik la Cour <frediklac@gmail.com>
2019-03-18 11:43:16 +0100

    circle

--- sketch.js
+++ sketch.js
@@ -1,14 +1,8 @@
 
 function setup() {
-  // This paragraph is created aside of the main block of code.
-  // It's to differentiate the creation of an element from its
-  // selection. Selected elements don't need to be created by
-  // p5js, they can be just plain HTML.
-  createP(
-    'I learn in this Letter, that Don Peter of Aragon, ' +
-      ' comes this night to Messina'
-  ).addClass('text').hide();
+createCanvas(windowWidth,windowHeight);
+background(0);
 
 function draw() {
-
+ellipse(50,50,50,50);
 }
